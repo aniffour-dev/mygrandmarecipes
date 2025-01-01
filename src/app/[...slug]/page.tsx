3e621649f4b571a584e27a8fa3ba391d82008598
Hamza Aniffour <aniffourmersh@gmail.com>
2025-01-01 18:39:21 +0100

    Dynamic Page Fixed

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -6,9 +6,9 @@ import Article from "@/app/components/Dynamic/Contents/Article";
 import Page from "@/app/components/Dynamic/Contents/Page";
 import Category from "@/app/components/Dynamic/Contents/Category";
 
-type Props = {
-  params: { slug: string[] };
-};
+interface Props {
+  params: Promise<{ slug: string[] }>;
+}
 
 // Define a type for Post
 interface Post {
@@ -33,7 +33,8 @@ async function fetchContent(slug: string[]) {
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const content = await fetchContent(params.slug);
+  const { slug } = await params;
+  const content = await fetchContent(slug);
   if (!content) {
     return {
       title: "Not Found",
@@ -45,7 +46,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     content.description ||
     (content.content ? content.content.substring(0, 160) : "");
 
-  const canonicalUrl = `https://www.foudrecipes.com/${params.slug.join("/")}`;
+  const canonicalUrl = `https://mygrandmarecipes.com/${slug.join("/")}`;
 
   switch (content.type) {
     case "category":
@@ -137,7 +138,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export default async function DynamicPage({ params }: Props) {
-  const content = await fetchContent(params.slug);
+  const { slug } = await params;
+
+  const content = await fetchContent(slug);
   if (!content) notFound();
 
   // Schema Markup
@@ -180,7 +183,7 @@ export default async function DynamicPage({ params }: Props) {
               item: {
                 "@type": "BlogPosting",
                 name: post.title,
-                url: `/${post.slug}`,
+                url: `${post.slug}`,
               },
             }))
           : [],
